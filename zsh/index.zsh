7b1d7deb86380609c763be28848ee3ae27af558c
Josh Curtiss <josh@curtiss.me>
2026-07-17 11:21:20 -0500

    feat: Add a master source script

---
+++ zsh/index.zsh
@@ -0,0 +1,20 @@
+DIR_PATH="${0:a:h}"
+
+# Prioritize these systems first
+source $DIR_PATH/brew.zsh
+
+# Everything else
+source $DIR_PATH/aliases.zsh
+source $DIR_PATH/asciinema.zsh
+source $DIR_PATH/cd.zsh
+source $DIR_PATH/docker.zsh
+source $DIR_PATH/ffmpeg.zsh
+source $DIR_PATH/git.zsh
+source $DIR_PATH/history.zsh
+source $DIR_PATH/ls.zsh
+source $DIR_PATH/network.zsh
+source $DIR_PATH/nvm.zsh
+source $DIR_PATH/rm.zsh
+source $DIR_PATH/suggest.zsh
+source $DIR_PATH/tunnel_container.zsh
+source $DIR_PATH/typos.zsh
