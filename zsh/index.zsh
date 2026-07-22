d836c1a64c2610c7a28bf79e26d3ac75437832d6
Josh Curtiss <josh@curtiss.me>
2026-07-21 22:56:42 -0500

    feat: Add autoload and suffix alias handlers to master source script

--- zsh/index.zsh
+++ zsh/index.zsh
@@ -2,6 +2,7 @@ DIR_PATH="${0:a:h}"
 
 # Prioritize these systems first
 source $DIR_PATH/brew.zsh
+source $DIR_PATH/autoload.zsh
 
 # Everything else
 source $DIR_PATH/aliases.zsh
@@ -15,6 +16,7 @@ source $DIR_PATH/ls.zsh
 source $DIR_PATH/network.zsh
 source $DIR_PATH/nvm.zsh
 source $DIR_PATH/rm.zsh
+source $DIR_PATH/suffix_aliases.zsh
 source $DIR_PATH/suggest.zsh
 source $DIR_PATH/tunnel_container.zsh
 source $DIR_PATH/typos.zsh
